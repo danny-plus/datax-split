586b177fe00683a8ccb0d34a356b6d140e8eb1f8
danny_ni <me@danny-ni.com>
2020-09-27 12:53:02 +0800

    修正重复START的问题

--- datax-split-hbase11reader/src/main/java/ni/danny/dataxsplit/hbase11/HBase11Key.java
+++ datax-split-hbase11reader/src/main/java/ni/danny/dataxsplit/hbase11/HBase11Key.java
@@ -6,4 +6,5 @@ public class HBase11Key extends Key {
     public final static String HBASE11_READER="hbase11xreader";
     public final static String RANGE_START="range.startRowkey";
     public final static String RANGE_END="range.endRowkey";
+    public final static String HBASE_TABLE_SPLITS="hbase.tableSpits";
 }
